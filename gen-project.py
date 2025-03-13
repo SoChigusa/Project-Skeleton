920cd5a4f5d01526b2a1fa803446abf5e65b16c5
So Chigusa <so.chigusa.pp@gmail.com>
2025-03-12 21:23:08 -0400

    added linux mode

--- gen-project.py
+++ gen-project.py
@@ -7,8 +7,18 @@ if len(sys.argv) <= 1:
     exit()
 else:
     projname=sys.argv[1]
-shutil.copytree('/Users/sochigusa/works/Project-Skeleton/tmp-proj', '/Users/SoChigusa/works/'+projname)
-os.chdir('/Users/SoChigusa/works/'+projname)
+
+sys_os = sys.platform
+if sys_os == 'darwin':
+    work_dir = '/Users/SoChigusa/works'
+elif sys_os == 'linux' or sys_os == 'linux2':
+    work_dir = '/home/sochigusa/works'
+else:
+    print('MacOS X / Linux is expected')
+    exit()
+
+shutil.copytree(work_dir+'/Project-Skeleton/tmp-proj', work_dir+'/'+projname)
+os.chdir(work_dir+'/'+projname)
 subprocess.run(['git','init'])
 subprocess.run(['git','add','*'])
 subprocess.run(['git','commit','-a','-m','"First commit"'])
